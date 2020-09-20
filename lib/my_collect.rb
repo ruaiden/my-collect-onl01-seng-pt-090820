ccd7411251c1e910281dd6def173e4f62b8fa0d6
Aiden Marques <aiden.marques94@gmail.com>
2020-09-20 04:51:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,8 @@ def my_collect(collection)
   i = 0
   collector = []
   while i < collection.length
-   collector << yield (collection[i])
-  
- i += 1 
+    yield (collection[i])
+  i += 1 
   
 end
 
