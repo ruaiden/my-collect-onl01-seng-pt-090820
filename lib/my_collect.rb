b6f0fdd73b386048b8416119525bd18ec17cec21
Aiden Marques <aiden.marques94@gmail.com>
2020-09-20 04:53:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,11 @@ def my_collect(collection)
   i = 0
   collector = []
   while i < collection.length
-    yield (collection[i])
+    yield (collection.upcase[i])
   i += 1 
   
 end
 
-  collector << collection.upcase
+  
 
 end
